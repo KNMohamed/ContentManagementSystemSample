8b5c22a9ba814db3fc2357df1318a2ba54cdf22e
yourmaingotoguy <Khalid.N.Mohamed@outlook.com>
2019-08-24 19:55:36 -0400

    -first commit

---
+++ public/staff/pages/show.php
@@ -0,0 +1,16 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+<?php
+    $id = $_GET['id'] ?? 1;
+?>
+
+<?php include(SHARED_PATH . '/staff_header.php')?>
+
+<div id="content">
+    <div class="back-link"><a class="action" href="<?php echo url_for('/staff/pages/index.php');?>">&laquo; Back to list</a></div>
+    <div class="page show">
+        Page ID: <?php echo h($id);?>
+    </div>
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php')?>
